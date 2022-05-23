696b8940e1a2040ccae2b4ed21929fb0a7c38c47
tian-cthit <43132597+tian-cthit@users.noreply.github.com>
2022-05-23 18:25:14 +0800

    state machine

--- state_machine/state_machine.py
+++ state_machine/state_machine.py
@@ -1,1 +1,17 @@
+import json
+
+
 class StateMachine:
+    def __init__(self, state_json, init_state):
+        self.current_state = init_state
+        self.state_history = [self.current_state]
+        with open(state_json) as state_json:
+            self.state_dict = json.load(state_json)
+
+    def transition(self, new_state):
+        if new_state not in self.state_dict[self.current_state]:
+            raise ValueError('The new state is not a valid downstream state')
+        else:
+            self.current_state = new_state
+            self.state_history.append(self.current_state)
+
